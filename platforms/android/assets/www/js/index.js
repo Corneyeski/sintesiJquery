c717d55ac1f718540e18607c989fc24bd0e39f69
alanv <alanvallve@gmail.com>
2018-01-29 18:32:10 +0100

    Examen

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -8,6 +8,12 @@ var guardar;
 var elements = 0;
 var myVar;
 var score = 0;
+var sameTime = 1;
+
+
+//EXAMEN
+var time = 1000;
+
 var app =
     {
         initialize: function () {
@@ -48,12 +54,9 @@ var app =
             $("#keep").click(function () {
                 var watchID;
                 var options = {
-                    frequency: 1000
+                    frequency: 2000
                 };
                 watchID = navigator.compass.watchHeading(onSuccess, onError, options);
-                /*console.log(
-                    navigator.compass.getCurrentHeading() + " aqui");*/
-
             });
 
             $("#score").css("width", window.screen.width);
@@ -67,8 +70,33 @@ var app =
                 score = 0;
                 elements = 0;
                 $("#game").empty();
+                updating();
                 myVar = setInterval(myTimer, 1000);
             })
+
+
+            //EXAMEN
+
+            //1 - OK
+            $("#change").click(function () {
+                $("#change").attr("class","btn btn-danger");
+                navigator.vibrate(1500)
+                window.setTimeout( returnClass, 1500 );
+            });
+            //2 - OK
+            $("#first").click(function () {
+                navigator.vibrate(1000)
+                createButton();
+            });
+            //3 -
+            $("#sud").click(function () {
+                var watchID2;
+                var options2 = {
+                    frequency: 1000
+                };
+                watchID2 = navigator.compass.watchHeading(onSuccess2, onError, options2);
+            })
+
         },
 
 
@@ -82,40 +110,46 @@ var app =
 
 function myTimer() {
 
-    let random = Math.floor((Math.random() * 3) + 1);
+    var count = 0;
+    while(count < sameTime) {
+        count++;
+        let random = Math.floor((Math.random() * 3) + 1);
 
-    var div;
-    switch (random) {
-        case 1:
-             div = $("<div class='target'></div>");
-            break;
-        case 2:
-             div = $("<div class='end'></div>");
-            break;
-        case 3:
-             div = $("<div class='clear'></div>");
-            break;
+        var div;
+        switch (random) {
+            case 1:
+                div = $("<div class='target'></div>");
+                break;
+            case 2:
+                div = $("<div class='end'></div>");
+                break;
+            case 3:
+                div = $("<div class='clear'></div>");
+                break;
+        }
+
+        var one = $("#game").width() - 45;
+        var two = $("#game").height() - 45;
+
+        //console.log("geeey: " + one + " heeey: " + two)
+
+        let left = Math.floor(Math.random() * one);
+        let top = Math.floor(Math.random() * two);
+
+        //console.log("left: " + left + " top: " + top)
+
+        div.css({"margin-left": left, "margin-top": top});
+
+        $("#game").append(div);
+
+        div.click(remove);
+        elements++;
+        updating();
+
+        if (elements == 11) clearInterval(myVar);
     }
 
-    var one =$("#game").width()-45;
-    var two = $("#game").height()-45;
-
-    //console.log("geeey: " + one + " heeey: " + two)
-
-    let left = Math.floor(Math.random() * one);
-    let top = Math.floor(Math.random() * two);
-
-    //console.log("left: " + left + " top: " + top)
-
-    div.css({"margin-left": left, "margin-top": top});
-
-    $("#game").append(div);
-
-    div.click(remove);
-
-    elements++;
-
-    if(elements == 11) clearInterval(myVar);
+    if (sameTime < 6)sameTime++;
 }
 
 function onSuccess(heading) {
@@ -134,7 +168,6 @@ function onError(compassError) {
     alert('Compass error: ' + compassError.code);
 }
 
-
 function remove() {
     var type = $(this).attr("class");
     switch (type) {
@@ -152,7 +185,46 @@ function remove() {
             $("#game").empty();
             break;
     }
+    updating();
+
     console.log("puntos: " + score + " elements: " + elements);
     $(this).remove();
+    sameTime--;
 }
+function updating() {
+    $("#progress").animate({
+        width: elements+"0%"
+    },500);
+
+    $("#numDanger").text(elements);
+    $("#points").text("POINTS:" + score);
+}
+
+//EXAMEN
+function returnClass() {
+    $("#change").attr("class","btn btn-warning");
+}
+
+function createButton() {
+    time += 1000;
+    var div = $("<div class='btn btn-primary'>"+ time +"</div>");
+
+    div.click(vibrarX);
+
+    $("#buttons").append(div);
+}
+function vibrarX() {
+    navigator.vibrate($(this).text());
+    createButton();
+}
+
+function onSuccess2(heading) {
+    console.log(heading);
+
+    if(heading.magneticHeading < 5 && heading.magneticHeading > -5){
+        navigator.notification.beep(2);
+    }
+
+}
+
 app.initialize();
